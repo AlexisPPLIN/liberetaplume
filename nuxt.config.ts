5169a06cafca23e9f334473b27cad7c43c678dfa
AlexisPPLIN <contact@alexispoupelin.com>
2022-12-30 13:14:32 +0100

    Added meta image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,8 @@ export default defineNuxtConfig({
             charset: 'utf-8',
             title: 'Libère ta plume - Graphothérapeute',
             meta: [
-                { name: 'description', content: 'Libère ta plume est un cabinet de Graphothérapie.' }
+                { name: 'description', content: 'Libère ta plume est un cabinet de Graphothérapie.' },
+                { name: 'image', content: '/img/logo.png' }
             ],
             link: [
                 { rel: 'icon', type: 'image/svg+xml', href: '/img/icons/feather.svg' }
