fd5cd55dfb6930f96785cf689f6ee18692216e53
AlexisPPLIN <contact@alexispoupelin.com>
2022-12-30 13:17:31 +0100

    Fixed typo for img meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
             title: 'Libère ta plume - Graphothérapeute',
             meta: [
                 { name: 'description', content: 'Libère ta plume est un cabinet de Graphothérapie.' },
-                { name: 'image', content: '/img/logo.png' }
+                { name: 'image', content: 'img/logo.png' }
             ],
             link: [
                 { rel: 'icon', type: 'image/svg+xml', href: '/img/icons/feather.svg' }
