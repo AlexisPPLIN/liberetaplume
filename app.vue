ecf799e548fbbf0189b3bb44c3094d856d0f7792
Alexis <contact@alexispoupelin.com>
2023-02-10 20:02:04 +0100

    Updated contacts + SEO

--- app.vue
+++ app.vue
@@ -114,38 +114,4 @@ const loading = ref(true);
 nuxtApp.hook("page:finish", () => {
   loading.value = false;
 });
-
-useSchemaOrg([
-  defineLocalBusiness({
-    name: 'Libère ta plume - Graphothérapie',
-    logo: '/img/logo.png',
-    telephone: '07 83 75 15 83',
-    email: 'contact@liberetaplume.fr',
-    address: {
-      addressCountry: 'FR',
-      postalCode: '85500',
-      streetAddress: '76 Rue Yves Ramoz',
-      addressLocality: 'Saint-Paul-en-Pareds'
-    },
-    openingHoursSpecification: [
-      {
-        dayOfWeek: 'Wednesday',
-        opens: '09:00',
-        closes: '12:00',
-      },
-      {
-        dayOfWeek: 'Wednesday',
-        opens: '14:00',
-        closes: '18:00',
-      },
-      {
-        dayOfWeek: 'Saturday',
-        opens: '09:00',
-        closes: '12:00',
-      },
-    ]
-  }),
-  defineWebSite(),
-  defineWebPage(),
-])
 </script>
--- pages/contacts.vue
+++ pages/contacts.vue
@@ -3,12 +3,34 @@
     <h1>Me contacter</h1>
 
     <div class="icons">
-      <a class="icon" href="#" target="_blank"><img src="/img/icons/google-maps.svg" alt="Fiche Google Maps de Libère ta plume"/></a>
-      <a class="icon" href="#" target="_blank"><img src="/img/icons/instagram.svg" alt="Page instagram de Libère ta plume"/></a>
-      <a class="icon" href="https://www.facebook.com/profile.php?id=100089095176948" target="_blank"><img src="/img/icons/facebook.svg" alt="Page Facebook de Libère ta plume"/></a>
-      <a class="icon" href="#" target="_blank"><img src="/img/icons/linkedin.svg" alt="Page Linkedin de Libère ta plume"/></a>
-      <a class="icon" href="mailto:contact@liberetaplume.fr" target="_blank"><img src="/img/icons/email-outline.svg" alt="Email de Libère ta plume"/></a>
-      <a class="icon" href="tel:+33783751583"><img src="/img/icons/phone.svg" alt="Numéro de téléphone de Libère ta plume"/></a>
+      <a class="icon" href="#" target="_blank" v-if="false">
+        <img src="/img/icons/google-maps.svg" alt="Fiche Google Maps de Libère ta plume"/>
+        <span>Google Maps</span>
+      </a>
+
+      <a class="icon" href="#" target="_blank" v-if="false">
+        <img src="/img/icons/instagram.svg" alt="Page instagram de Libère ta plume"/>
+        <span>Instagram</span>
+      </a>
+      
+      <a class="icon" href="https://www.facebook.com/profile.php?id=100089095176948" target="_blank">
+        <img src="/img/icons/facebook.svg" alt="Page Facebook de Libère ta plume"/>
+        <span>Facebook</span>
+      </a>
+
+      <a class="icon" href="#" target="_blank" v-if="false">
+        <img src="/img/icons/linkedin.svg" alt="Page Linkedin de Libère ta plume"/>
+        <span>Linkedin</span>
+      </a>
+
+      <a class="icon" href="mailto:contact@liberetaplume.fr" target="_blank">
+        <img src="/img/icons/email-outline.svg" alt="Email de Libère ta plume"/>
+        <span>contact@liberetaplume.fr</span>
+      </a>
+      <a class="icon" href="tel:+33783751583">
+        <img src="/img/icons/phone.svg" alt="Numéro de téléphone de Libère ta plume"/>
+        <span>07 83 75 15 83</span>
+      </a>
     </div>
     
     <Nav />
@@ -17,22 +39,28 @@
 </template>
 
 <style scoped>
+  h1 {
+    position: fixed;
+    top: 2%;
+  }
+
   .icons {
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    width: auto;
+    width: 100%;
+    flex-wrap: wrap;
   }
 
   .icon {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     height: 4em;
-    width: 4em;
     color: black;
+    padding: 3%;
   }
 
   .icon img {
@@ -45,8 +73,20 @@
     opacity: 100%;
     transform: scale(1.5);
   }
+
+  .icon span {
+    text-decoration: none !important;
+    font-size: 1.5em;
+  }
 </style>
 
 <script setup>
+  useSchemaOrg([
+    defineWebPage({
+      '@type': 'ContactPage',
+      'name': 'Me contacter',
+      'description': 'Contactez-moi par téléphone, email ou sur les réseaux sociaux'
+    })
+  ]);
 
 </script>
